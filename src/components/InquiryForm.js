1fb247d9e77259c1eec1c64e5d1a180e0a563f8f
Charlie <ctwinnard@gmail.com>
2018-12-01 10:09:39 -0700

    style form

--- src/components/InquiryForm.js
+++ src/components/InquiryForm.js
@@ -3,22 +3,27 @@ import React from 'react';
 const InquiryForm = () => (
     <div className="InquiryForm">
         <form action="https://charlie-system.herokuapp.com" method="post">
+            <h4> Please describe your request </h4>
             <label htmlFor="email">
-                Email:
+                Email*:
                 <input type="email" name="email" required />
             </label>
+            <br />
             <label htmlFor="businessType">
                 Business Type:
                 <input type="text" name="businessType" />
             </label>
+            <br />
             <label htmlFor="businessDescription">
-                Business Description:
+                Business Description*:
                 <input type="text" name="businessDescription" required />
             </label>
+            <br />
             <label htmlFor="request">
-                Describe your app request:
+                Describe your app request*:
                 <input type="text" name="request" required />
             </label>
+            <br />
             <input type="submit" value="Submit" />
         </form>
     </div>
