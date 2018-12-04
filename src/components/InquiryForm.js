d4d0f1e30f615ec669682431b55a1a6c4178f9ee
Charlie <ctwinnard@gmail.com>
2018-12-03 18:17:10 -0700

    mui first pass

--- src/components/InquiryForm.js
+++ src/components/InquiryForm.js
@@ -1,32 +1,49 @@
+/* eslint react/prefer-stateless-function: 0 */
+
+// import axios from 'axios';
+import { FormControl } from '@material-ui/core';
 import React from 'react';
 
+// handleSubmit = (event) => {
+//     axios.post('https://charlie-system.herokuapp.com/inquiry/new', {}).then(() => {
+//         console.log('success');
+//     }, (e) => {
+//         console.log(e);
+//     });
+// }
+
 const InquiryForm = () => (
-    <div className="InquiryForm">
-        <form action="https://charlie-system.herokuapp.com/inquiry/new" method="post">
-            <h4> Please describe your request </h4>
-            <label htmlFor="email">
-                Email*:
-                <input type="email" name="email" required />
-            </label>
-            <br />
-            <label htmlFor="businessType">
-                Business Type*:
-                <input type="text" name="businessType" required />
-            </label>
-            <br />
-            <label htmlFor="businessDescription">
-                Business Description:
-                <input type="text" name="businessDescription" />
-            </label>
-            <br />
-            <label htmlFor="request">
-                Describe your app request*:
-                <input type="text" name="request" required />
-            </label>
-            <br />
-            <input type="submit" value="Submit" />
-        </form>
-    </div>
+    <FormControl>
+        hello form
+    </FormControl>
+    // <div className="InquiryForm">
+    //     <form onSubmit={onSubmit()}>
+    //         <h4> Please describe your request </h4>
+    //         <label htmlFor="email">
+    //             Email*:
+    //             <input type="email" name="email" required />
+    //         </label>
+    //         <br />
+    //         <label htmlFor="businessType">
+    //             Business Type*:
+    //             <input type="text" name="businessType" required />
+    //         </label>
+    //         <br />
+    //         <label htmlFor="businessDescription">
+    //             Business Description:
+    //             <input type="text" name="businessDescription" />
+    //         </label>
+    //         <br />
+    //         <label htmlFor="request">
+    //             Describe your app request*:
+    //             <input type="text" name="request" required />
+    //         </label>
+    //         <br />
+    //         <input type="submit" value="Submit" />
+    //     </form>
+    // </div>
 );
+    // }
+// }
 
 export default InquiryForm;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
 /* eslint-disable */
 
+import { createMuiTheme,  MuiThemeProvider } from '@material-ui/core';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({palette:
+    {
+      primary: {
+        light: 'white',
+        main: 'red',
+        dark: 'black',
+        contrastText: 'purple',
+      },
+      secondary: {
+        light: 'white',
+        main: 'red',
+        dark: 'black',
+        contrastText: 'purple',
+      },
+      error: {
+        light: 'white',
+        main: 'red',
+        dark: 'black',
+        contrastText: 'purple',
+      },
+  }
+})
+
+ReactDOM.render(
+    <MuiThemeProvider theme={theme} >
+        <App />
+    </MuiThemeProvider>,
+    document.getElementById('root')
+);
