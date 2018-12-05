6d25e384125af31d12c83553db955eef351208b9
Charlie <ctwinnard@gmail.com>
2018-12-05 16:58:30 -0700

    app bar style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 /* eslint react/jsx-one-expression-per-line: 0 */
 /* eslint react/prefer-stateless-function: 0 */
 
+import AppBar from '@material-ui/core';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './App.css';
@@ -11,6 +12,7 @@ class App extends Component {
     render() {
         return (
             <div className="App">
+                <AppBar />
                 <InquiryForm />
             </div>
         );
