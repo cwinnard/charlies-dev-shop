34425fb17f7b18ca7672e2fccd9d7c901c49aa1a
Charlie <ctwinnard@gmail.com>
2018-12-05 17:03:28 -0700

    app bar style

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <AppBar position="static" color="default"/>
+                <AppBar position="static" color="default" />
                 <InquiryForm />
             </div>
         );
