82d928f9928369f61b1fe59e691b4f3f6cadd91d
Charlie <ctwinnard@gmail.com>
2018-12-15 11:19:12 -0700

    signup form

--- src/components/functional/SignupForm.js
+++ src/components/functional/SignupForm.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import {
     Button,
     Card,
@@ -6,6 +7,12 @@ import {
     TextField,
 } from '@material-ui/core';
 import React from 'react';
+
+const NEW_USER_URL = 'https://charlie-system.herokuapp.com/users/new';
+
+const postForm = () => {
+    axios.post(NEW_USER_URL, {});
+};
 
 const SignupForm = () => (
     <Card>
@@ -20,6 +27,7 @@ const SignupForm = () => (
                     color="primary"
                     variant="contained"
                     size="large"
+                    onClick={postForm}
                 >
                     Start
                 </Button>
