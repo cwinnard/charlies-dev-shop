fc0d112a3fcfaff1a8dbb7d7218c79f92b22e257
Charlie <ctwinnard@gmail.com>
2018-12-15 11:35:38 -0700

    menu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 /* eslint react/jsx-one-expression-per-line: 0 */
 /* eslint react/prefer-stateless-function: 0 */
 
+import { AppBar } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
+
 import './App.css';
 
-import MenuBar from './components/functional/MenuBar';
+import MenuDrawer from './components/functional/MenuDrawer';
 import DataTablePage from './components/stateful/DataTablePage';
 import EmailSignupPage from './components/stateful/EmailSignupPage';
 import InquiryPage from './components/stateful/InquiryPage';
 import LandingPage from './components/stateful/LandingPage';
 import ProductPage from './components/stateful/ProductPage';
+import toggleMenu from './redux/actions/menuActions';
 
+const SHOPNAME = 'Charlie\'s Dev Shop';
 
 class App extends Component {
     render() {
+        const { menuVisible, toggle } = this.props;
         return (
             <div className="App">
-                <MenuBar />
+                <AppBar className="header" position="static" color="primary">
+                    <div className="menuContainer">
+                        <div className="menu">
+                            <MenuIcon onClick={toggle} />
+                            <MenuDrawer
+                                open={menuVisible}
+                                onClose={toggle}
+                            />
+                        </div>
+                        <div className="shopName">
+                            {SHOPNAME}
+                        </div>
+                    </div>
+                </AppBar>
                 <Switch>
                     <Route exact path="/" component={LandingPage} />
                     <Route exact path="/ecommerce" component={ProductPage} />
@@ -31,4 +51,24 @@ class App extends Component {
     }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+    console.log(state);
+    const { menuVisible } = state.app;
+    console.log(menuVisible);
+    return {
+        menuVisible,
+    };
+};
+
+const mapDispatchToProps = dispatch => (
+    {
+        toggle: () => { dispatch(toggleMenu()); },
+    }
+);
+
+App.propTypes = {
+    menuVisible: PropTypes.bool.isRequired,
+    toggle: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/components/stateful/Dashboard.js
@@ -0,0 +1,16 @@
+/* eslint react/jsx-one-expression-per-line: 0 */
+/* eslint react/prefer-stateless-function: 0 */
+
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+                Hello Dashboard
+            </div>
+        );
+    }
+}
+
+export default Dashboard;
